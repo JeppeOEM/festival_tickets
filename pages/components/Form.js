f4bed3b98466bc5c4609e065a9e30b0b7432a6bf
Aboood <abdu3793@stud.kea.dk>
2022-12-06 14:58:31 +0100

    build billing form

--- pages/components/Form.js
+++ pages/components/Form.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import Participants from "./Participants";
 
 const Form = () => {
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  let number = 10;
   const CheakoutData = [
     {
       fullname: "",
@@ -8,18 +13,22 @@ const Form = () => {
     },
   ];
   const [formFields, setFormFields] = useState(CheakoutData);
+
+  if (formFields.length === 1) {
+    setDisabled.disabled === true;
+  }
   const handleFormChange = (e, index) => {
     let data = [...formFields];
     data[index][e.target.name] = e.target.value;
     setFormFields(data);
   };
-  if (formFields.length === 4) {
-  }
   const addFields = () => {
     let object = {
+      fullname: "",
       age: "",
     };
     setFormFields([...formFields, object]);
+    increase();
   };
   // const removeField = (index) => {
   //   let data = [...formFields];
@@ -30,12 +39,16 @@ const Form = () => {
     e.preventDefault();
     console.log("grab da data", formFields);
   };
+  const increase = () => {
+    setCounter((counter) => counter + 1);
+  };
   return (
     <>
       {formFields.map((form, index) => {
         return (
           <div key={index}>
             <form onSubmit={submit}>
+              <label>Participant {counter}</label>
               <input name="fullname" placeholder="Participant Full Name" onChange={(e) => handleFormChange(e, index)} value={form.name} />
               <input name="age" placeholder="Participant age" onChange={(e) => handleFormChange(e, index)} value={form.age} />
               {/* <button onClick={() => removeField(index)}>delete participan</button> */}
@@ -43,8 +56,27 @@ const Form = () => {
           </div>
         );
       })}
-      <button onClick={addFields}>add participants</button>
-      <button onClick={submit}>next</button>
+      {formFields.length < number && <button onClick={addFields}>ADD PARTICIPANT</button>}
+
+      {formFields.length < number && (
+        <button disabled={true} onClick={submit}>
+          this is cool{" "}
+        </button>
+      )}
+
+      {formFields.length === number && (
+        <button disabled={false} onClick={submit}>
+          this is cool{" "}
+        </button>
+      )}
+
+      {/* {formFields.length === number && (
+        <button disabled={disabled} onClick={submit}>
+          GO TO CHECKOUT
+        </button>
+      )} */}
+      {/* <button disabled={disabled}>Click to Disable</button> */}
+      <Participants />
     </>
   );
 };
--- pages/components/Participants.js
+++ pages/components/Participants.js
@@ -1,15 +1,44 @@
+import { useState } from "react";
+
 const Participants = () => {
+  const [details, setDetails] = useState({
+    name: "",
+    email: "",
+    city: "",
+    postcode: "",
+    addresse: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDetails((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(details);
+  };
   return (
     <>
-      <form action="/send-data-here" method="post">
-        <label htmlFor="first">First name:</label>
-        <input type="text" id="first" name="first" />
-        <label htmlFor="last">Last name:</label>
-        <input type="text" id="last" name="last" />
-        <button type="submit">Submit</button>
-        <span>{}</span>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name And Surname<input type="text" name="name" onChange={handleChange}></input>
+        </label>
+        <label>
+          Email Adresse<input type="text" name="email" onChange={handleChange}></input>
+        </label>
+        <label>
+          City<input type="text" name="city" onChange={handleChange}></input>
+        </label>
+        <label>
+          Post Code<input type="number" name="postcode" onChange={handleChange}></input>
+        </label>
+        <label>
+          Addresse<input type="text" name="addresse" onChange={handleChange}></input>
+        </label>
+        <button type="submit">submit</button>
       </form>
-      ;
     </>
   );
 };
