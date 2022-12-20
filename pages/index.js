3799988320cefa9311b2bf89ca8eec750b3d0073
Jeppe <jepp026b@edu.zealand.dk>
2022-12-20 18:17:39 +0100

    test3

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import BandsLanding from "./components/BandsLanding";
+import BandsLanding from "../components/BandsLanding";
 import { Component } from "react";
 import { mountCounter } from "dynamic-countdown";
-import NavBooking from "./components/NavBooking";
-import Footer from "./components/Footer";
-import News from "./components/News";
+import NavBooking from "../components/NavBooking";
+import Footer from "../components/Footer";
+import News from "../components/News";
 import style from "../styles/component/landing.module.scss";
 import ui from "../styles/component/ui.module.scss";
 
@@ -22,11 +22,11 @@ export default function Home({ bands }) {
       </Head>
 
       <div className={style.topSec}>
-        <video className={style.video} src="/BgVid.mp4" autoPlay muted loop />
+        <video className={style.video} src='/BgVid.mp4' autoPlay muted loop />
         <NavBooking ass={"this"} />
         <div className={style.heading}>
           <h1>Ragnarök</h1>
-          <Link href="./tickets">
+          <Link href='./tickets'>
             <button className={ui.button}>buy tickets</button>
           </Link>
         </div>
@@ -58,7 +58,7 @@ export default function Home({ bands }) {
         <p>get the latset news about the line-up and the latest news about Ragnarök festival</p>
         <form>
           {/* <label>Newsletter</label> */}
-          <input type="text" name="newsletter" placeholder=" TYPE YOUR EMAIL"></input>
+          <input type='text' name='newsletter' placeholder=' TYPE YOUR EMAIL'></input>
           <button className={ui.button}>Subscribe</button>
         </form>
       </div>
